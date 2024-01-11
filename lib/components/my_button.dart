d31846c8f8318240a06dd3fc1900f6c97d9443a0
tfayzullaev <temurbek.fayzullaev@bankzamin.uz>
2024-01-11 22:04:44 +0500

    button

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -11,7 +11,7 @@ class MyButton extends StatelessWidget {
       onTap: onTap,
       child: Container(
         padding: const EdgeInsets.all(25),
-        margin: const EdgeInsets.symmetric(horizontal: 25),
+        margin: const EdgeInsets.symmetric(horizontal: 20),
         decoration: BoxDecoration(
           color: Colors.black,
           borderRadius: BorderRadius.circular(8),
